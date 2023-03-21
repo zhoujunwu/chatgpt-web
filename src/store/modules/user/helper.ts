66de5d061b4fc73c678a24d86d85baadfcfdaae1
jackzhou <jack.zhou.jz1@roche.com>
2023-03-22 01:13:03 +0800

    update author infor

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -12,12 +12,22 @@ export interface UserState {
   userInfo: UserInfo
 }
 
+// export function defaultSetting(): UserState {
+//   return {
+//     userInfo: {
+//       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
+//       name: 'ChenZhaoYu',
+//       description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+//     },
+//   }
+// }
+
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: '',
+      name: 'Hello',
+      description: '',
     },
   }
 }
